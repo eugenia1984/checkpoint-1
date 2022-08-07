e5fee395c5ad1290d376d3f790428a114094307f
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-08-07 17:11:08 -0300

    agregamos index, table, form y card

---
+++ scripts/cards.js
@@ -0,0 +1,25 @@
+const URL_CARDS = "https://rickandmortyapi.com/api/character";
+
+fetch(URL_CARDS)
+  .then((response) => response.json())
+  .then((data) => showData(data));
+
+showData = (data) => {
+  let body = ``;
+  const character = data.results;
+
+  character.map((user) => {
+    return (body += `
+        <div class="card mb-5" style="width: 18rem;">
+          <img src=${user.image} class="card-img-top" alt="${user.name}">
+          <div class="card-body">
+            <h5 class="card-title">${user.name}</h5>
+            <p class="card-text">Especie: ${user.species}</p>
+            <p class="card-text">Origen: ${user.origin.name}</p>
+            <p class="card-text">Creación: ${user.created}</p>
+          </div>
+        </div>
+        `);
+  });
+  document.getElementById("cards").innerHTML = body;
+};
---
+++ scripts/form.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form");
+
+const d = document;
+
+form.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  const user = d.getElementById("user").value;
+  const email = d.getElementById("email").value;
+  const comments = d.getElementById("comments").value; 
+  console.table(`user: ${user} \nemail: ${email} \ncomments: ${comments}`);
+});
+
